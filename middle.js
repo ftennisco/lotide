65c938292b33d64e51f06b9365277cee9859f7bd
ftennisco <f.tennisco@hotmail.com>
2023-05-20 16:04:39 -0700

    Implementing middle, lotide project

---
+++ middle.js
@@ -0,0 +1,32 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (Array.isArray(array1[i]) && (eqArrays(array1[i], array2[i]) === false)) {
+      return false;
+    }
+    if (!(Array.isArray(array1[i])) && array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArrayEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅  Assertion passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${array1} !== ${array2}`);
+  }
+};
+
+const middle = function(array) {
+  if (array.length <= 2) {
+    return [];
+  } else if (array.length % 2 === 0) {
+    return [array[Math.ceil(array.length / 2) - 1], array[Math.ceil(array.length / 2)]];
+  } else {
+    return [array[Math.floor(array.length / 2)]];
+  }
+};
