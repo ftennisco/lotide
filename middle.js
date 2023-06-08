28954af06274f75aa5ea85222232f64e856cf2c8
ftennisco <f.tennisco@hotmail.com>
2023-06-08 13:33:23 -0700

    added export.module to middle.js and created a test file in my test folder for middleTest.js

--- middle.js
+++ middle.js
@@ -1,26 +1,3 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (Array.isArray(array1[i]) && (eqArrays(array1[i], array2[i]) === false)) {
-      return false;
-    }
-    if (!(Array.isArray(array1[i])) && array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArrayEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅  Assertion passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🛑  Assertion failed: ${array1} !== ${array2}`);
-  }
-};
-
 const middle = function(array) {
   if (array.length <= 2) {
     return [];
@@ -30,3 +7,5 @@ const middle = function(array) {
     return [array[Math.floor(array.length / 2)]];
   }
 };
+
+module.exports = middle;
