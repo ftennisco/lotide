9fef16b0608e64eaa5ebc8b47abc97d42c015831
ftennisco <f.tennisco@hotmail.com>
2023-06-08 13:00:29 -0700

    added module exports to assertEqual and head js files. Created test files for the mentioned files.

--- assertEqual.js
+++ assertEqual.js
@@ -1,4 +1,4 @@
-const assertEqual = function(actual, expected) {
+const assertEquals = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
@@ -6,7 +6,5 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 20);
-assertEqual("friends", "friends");
+
+module.exports = assertEquals;
